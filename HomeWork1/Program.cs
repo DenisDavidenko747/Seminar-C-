7808399080b288906f6fc561b815c44652601715
Den <dr-den2@mail.ru>
2023-02-12 14:40:02 +0300

    Решил 2 задачу

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,22 +1,44 @@
-﻿Console.Write("Input a forst number: ");
+﻿// Console.Write("Input a forst number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// // int max = 0;
+// // int min = 0;
+
+// if(num1 > num2)
+// {
+//     // max = num1;
+//     // min = num2;
+//     Console.WriteLine("Max= " + num1 + " Min= " + num2);
+// }
+// else
+// {
+//     //  max = num2;
+//     //  min = num1;
+//     Console.WriteLine("Max= " + num2 + " Min= " + num1);
+// }
+
+// // Console.WriteLine("Max= " + max + " Min= " + min);
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Input a forst number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a last number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
 
-// int max = 0;
-// int min = 0;
+int max = num1;
 
-if(num1 > num2)
+if(num2 > max)
 {
-    // max = num1;
-    // min = num2;
-    Console.WriteLine("Max= " + num1 + " Min= " + num2);
+    max = num2;
 }
-else
+if(num3 > max)
 {
-    //  max = num2;
-    //  min = num1;
-    Console.WriteLine("Max= " + num2 + " Min= " + num1);
+    max = num3;
 }
 
-// Console.WriteLine("Max= " + max + " Min= " + min);
+Console.WriteLine(max);
