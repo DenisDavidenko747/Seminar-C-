faf5588fdea89bf29ee3cec0aeb30651e9021222
Den <dr-den2@mail.ru>
2023-02-12 14:33:27 +0300

    Решил 1 задачу

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Input a forst number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int max = 0;
+// int min = 0;
+
+if(num1 > num2)
+{
+    // max = num1;
+    // min = num2;
+    Console.WriteLine("Max= " + num1 + " Min= " + num2);
+}
+else
+{
+    //  max = num2;
+    //  min = num1;
+    Console.WriteLine("Max= " + num2 + " Min= " + num1);
+}
+
+// Console.WriteLine("Max= " + max + " Min= " + min);
