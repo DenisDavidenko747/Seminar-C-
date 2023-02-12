5294df3d637e7a362e93e087c75ea21ebb5ec3a8
Den <dr-den2@mail.ru>
2023-02-12 15:04:22 +0300

    Решил 3 задачу

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -23,22 +23,36 @@
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.Write("Input a forst number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a last number: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a forst number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a last number: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
 
-int max = num1;
+// int max = num1;
 
-if(num2 > max)
+// if(num2 > max)
+// {
+//     max = num2;
+// }
+// if(num3 > max)
+// {
+//     max = num3;
+// }
+
+// Console.WriteLine(max);
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0 )
 {
-    max = num2;
-}
-if(num3 > max)
+    Console.WriteLine("Да ");
+} 
+else
 {
-    max = num3;
+    Console.WriteLine("Нет ");
 }
-
-Console.WriteLine(max);
