54889e02378e26ebae6cefca9bc16df30871c12c
Den <dr-den2@mail.ru>
2023-02-21 03:09:36 +0300

    Домашняя работа готова

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,72 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// Console.Write("Input NUMBER: ");
+// string number = Console.ReadLine();
+// int N = number.Length;
+  
+// if (N == 5)
+// {
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"{number} - Палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{number} - Не палиндром");
+//     }
+// }
+//     else
+//     {
+//     Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+//     }
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// double Distance(double xA, double yA, double zA, double xB, double yB,  double zB)
+// {
+//     double distances = Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2) + Math.Pow(zB - zA, 2));
+//     return distances;
+// }
+
+// Console.Write("Input point coordinate xA: ");
+// double pointxA = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Input point coordinate yA: ");
+// double pointyA = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Input point coordinate zA: ");
+// double pointzA = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Input point coordinate xB: ");
+// double pointxB = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Input point coordinate yB: ");
+// double pointyB = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Input point coordinate zB: ");
+// double pointzB = Convert.ToDouble(Console.ReadLine());
+
+
+// double result = Distance(pointxA, pointyA, pointzB, pointxB, pointyB, pointzB);
+
+// Console.WriteLine(result);
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// void TablePrint(int num)
+// {
+//      int current = 1;
+
+//      while(current <= num)
+//     {
+//          Console.WriteLine(current + " " + Math.Pow(current, 3));
+//          current++;
+//     }
+// }
+
+//  Console.WriteLine("Enter your number");
+//  int number = Convert.ToInt32(Console.ReadLine());
+
+//  TablePrint(number);
+
+  
+
+ 
+ 
